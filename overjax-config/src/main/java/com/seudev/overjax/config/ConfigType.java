446599096694e39db5ea525e9c4706e7ef883c18
Thomás Sousa Silva <thomas.sousa.96@gmail.com>
2018-12-04 00:48:39 -0300

    Remove unused enum values (#34)

--- overjax-config/src/main/java/com/seudev/overjax/config/ConfigType.java
+++ overjax-config/src/main/java/com/seudev/overjax/config/ConfigType.java
@@ -1,30 +1,15 @@
 package com.seudev.overjax.config;
 
+import com.seudev.util.data.Enums;
+
 public enum ConfigType {
-
-	JSONB,
-
-	DEFAULT_MEDIA_TYPE,
-	WRAP_MEDIA_TYPES,
-
-	SHOW_STACK_TRACE,
-	STACK_TRACE_ON_STATUS_FAMILY,
-	STACK_TRACE_TEMPLATE,
-
-	CACHE_REQUEST_BODY,
-	CAHCE_REQUEST_BODY_WITH_BUFFER_SIZE,
-
-	EL_PROCESSOR;
-
-	private final String FULL_NAME;
-
-	private ConfigType() {
-		FULL_NAME = ConfigType.class.getName() + '.' + name();
-	}
-
-	@Override
-	public String toString() {
-		return FULL_NAME;
-	}
-
+    
+    JSONB,
+    EL_PROCESSOR;
+    
+    @Override
+    public String toString() {
+        return Enums.getFullName(this);
+    }
+    
 }
